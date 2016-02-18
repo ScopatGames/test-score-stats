e8370d2d92d17ef3131c9f184b007071ead463c1
Scott Patten <scopatten@gmail.com>
2016-02-17 17:09:23 -0700

    added browserSync to gulp

--- app/js/data.factory.js
+++ app/js/data.factory.js
@@ -1,33 +1,33 @@
-angular
-    .module('scoresApp')
-    .factory('dataservice', dataservice);
+(function (){
+    angular
+        .module('scoresApp')
+        .factory('dataservice', dataservice);
 
-dataservice.$inject = ['$window'];
+    function dataservice($window){
+        var service = {
+            getStudentData: getStudentData,
+            saveStudentData: saveStudentData
+        };
 
-function dataservice(){
-    var service = {
-        getStudentData: getStudentData,
-        saveStudentData: saveStudentData
-    };
-    
-    //Default student data
-    var defaultStudentData = [{
-            name: 'Sudsy McDerman',
-            score: 64.00
-        }, {
-            name: 'Boxtrod Folly',
-            score: 100.00
-    }];
-    
-    return service;
-    
-    function getStudentData(){
-        return ((typeof($window.Storage) !== "undefined" && localStorage.getItem('studentData')!==null) ? JSON.parse(localStorage.getItem('studentData')) : defaultStudentData);
-    }
-    
-    function saveStudentData(saveData){
-        if(typeof($window.Storage) !== "undefined"){
-            localStorage.setItem('studentData', JSON.stringify(saveData));
+        //Default student data
+        var defaultStudentData = [{
+                name: 'Sudsy McDerman',
+                score: 64.00
+            }, {
+                name: 'Boxtrod Folly',
+                score: 100.00
+        }];
+
+        return service;
+
+        function getStudentData(){
+            return ((typeof($window.Storage) !== "undefined" && localStorage.getItem('studentData')!==null) ? JSON.parse(localStorage.getItem('studentData')) : defaultStudentData);
+        }
+
+        function saveStudentData(saveData){
+            if(typeof($window.Storage) !== "undefined"){
+                localStorage.setItem('studentData', JSON.stringify(saveData));
+            }
         }
     }
-}
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
@@ -6,7 +7,10 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+    }));
 });
 
 gulp.task('lint', function() {
@@ -15,8 +19,16 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('browserSync', function(){
+   browserSync.init({
+       server: {
+           baseDir: 'app',
+       }
+   }); 
+});
 
 gulp.task('watch', ['sass', 'lint'], function(){
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/js/**/*.js', ['lint']);
 });
+
