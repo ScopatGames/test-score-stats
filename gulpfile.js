c176c53d67969e1bfd4aedb87c5cdeb5fc71c67c
Scott Patten <scopatten@gmail.com>
2016-02-17 17:46:00 -0700

    removed browserSync and added gulp-serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var serve = require('gulp-serve');
 
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-    }));
+        .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('lint', function() {
@@ -19,15 +16,11 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('browserSync', function(){
-   browserSync.init({
-       server: {
-           baseDir: 'app',
-       }
-   }); 
-});
+gulp.task('serve', serve({
+    root: 'app',
+    port: 3000}));
 
-gulp.task('watch', ['sass', 'lint'], function(){
+gulp.task('watch', ['serve', 'sass', 'lint'], function(){
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/js/**/*.js', ['lint']);
 });
